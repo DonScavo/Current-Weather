3b10a46b3ce5577da99cd53ff0c5d3a4088f06d0
Eduardo Scavo <edu.scavo@gmail.com>
2020-05-09 15:53:48 -0500

    Validaciónes del form, limpiar el codigo

--- js/app.js
+++ js/app.js
@@ -2,20 +2,21 @@ const formBuscador = document.getElementById('formBuscador');
 const buscador = document.getElementById('buscador');
 const search = document.getElementById('search');
 const resultado = document.getElementById('resultado');
+const alert = document.querySelector('.alert');
 
 
-formBuscador.addEventListener('submit', (e) => {
+formBuscador.addEventListener('submit', e => {
     e.preventDefault();
 
-    if (buscador === '') {
+    if (buscador.value === '') {
         console.log('Inserta una ciudad para ver el clima');
+        alert.textContent = "Inserte una ciudad para consultar el clima";
+        setTimeout(() => {
+            alert.textContent = "";
+        }, 2000)
     } else {
         const ciudad = buscador.value;
-
-        consultarAPI(ciudad)
-            .then(data => {
-                this.resultado.innerHTML = `<p> Ciudad: ${city} : ${data.respuesta.main.temp}</p> `;
-            })
+        consultarAPI(ciudad);
     }
 })
 
@@ -23,12 +24,27 @@ formBuscador.addEventListener('submit', (e) => {
 // Consulta asyncrona a la api
 async function consultarAPI(city) {
 
-    const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=308bbc0e3827c70548c75bf344117eb5&lang=sp`);
+    const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=308bbc0e3827c70548c75bf344117eb5&lang=sp&units=metric`);
+    if (url.status === 404) {
+        alert.textContent = "Esa ciudad no existe (que yo sepa), ¡intenta con otra!";
+        setTimeout(() => {
+            alert.textContent = "";
+        }, 2000)
+    } else if (url.status === 200) {
+        const respuesta = await url.json();
 
-    const respuesta = await url.json();
-    console.log(respuesta)
-    return {
-        respuesta
+        draw(respuesta);
+
     }
 }
 
+function draw(data) {
+    console.log(data)
+    resultado.innerHTML = `<p> Ciudad: ${data.name} <br>
+    Estado: ${data.weather[0].description} <br>
+    Temperatura: ${data.main.temp}°C <br>
+    Sensación termica: ${data.main.feels_like}°C <br>
+    Humedad: ${data.main.humidity}% <br>
+    Viento: ${data.wind.speed} M/S
+    </p> `;
+}
