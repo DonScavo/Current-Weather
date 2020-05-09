08d32388adc73fd8e6bc309ea5d2262f33e77f5e
Eduardo Scavo <edu.scavo@gmail.com>
2020-05-09 18:09:57 -0500

    Gráfica de temperatura minima y maxima agregada

--- js/app.js
+++ js/app.js
@@ -1,50 +1,49 @@
-const formBuscador = document.getElementById('formBuscador');
-const buscador = document.getElementById('buscador');
-const search = document.getElementById('search');
-const resultado = document.getElementById('resultado');
-const alert = document.querySelector('.alert');
+window.addEventListener('load', () => {
+
+    const formBuscador = document.getElementById('formBuscador');
+    const buscador = document.getElementById('buscador');
+    const resultado = document.getElementById('resultado');
+    const alert = document.querySelector('.alert');
 
 
-formBuscador.addEventListener('submit', e => {
-    e.preventDefault();
 
-    if (buscador.value === '') {
-        console.log('Inserta una ciudad para ver el clima');
-        alert.textContent = "Inserte una ciudad para consultar el clima";
-        setTimeout(() => {
-            alert.textContent = "";
-        }, 2000)
-    } else {
-        const ciudad = buscador.value;
-        consultarAPI(ciudad);
+    formBuscador.addEventListener('submit', e => {
+        e.preventDefault();
+        if (buscador.value === '') {
+            console.log('Inserta una ciudad para ver el clima');
+            alert.textContent = "Inserte una ciudad para consultar el clima";
+            setTimeout(() => {
+                alert.textContent = "";
+            }, 2500)
+        } else {
+            const ciudad = buscador.value;
+            consultarAPI(ciudad);
+        }
+    });
+
+    async function consultarAPI(city) {
+        const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=308bbc0e3827c70548c75bf344117eb5&lang=sp&units=metric`);
+        if (url.status === 404) {
+            alert.textContent = "Esa ciudad no existe (que yo sepa), ¡intenta con otra!";
+            setTimeout(() => {
+                alert.textContent = "";
+            }, 3000)
+        } else if (url.status === 200) {
+            const respuesta = await url.json();
+            draw(respuesta);
+        }
+    };
+
+    function draw(data) {
+        resultado.innerHTML = `<p>  Ciudad: ${data.name} <br>
+                                    Estado: ${data.weather[0].description} <br>
+                                    Temperatura: ${data.main.temp}°C <br>
+                                    Sensación termica: ${data.main.feels_like}°C <br>
+                                    Humedad: ${data.main.humidity}% <br>
+                                    Viento: ${data.wind.speed} M/S
+                                </p> `;
+        const dataChart = [data.main.temp_max, data.main.temp_min];
+        drawChart(dataChart);
     }
-})
+});
 
-
-// Consulta asyncrona a la api
-async function consultarAPI(city) {
-
-    const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=308bbc0e3827c70548c75bf344117eb5&lang=sp&units=metric`);
-    if (url.status === 404) {
-        alert.textContent = "Esa ciudad no existe (que yo sepa), ¡intenta con otra!";
-        setTimeout(() => {
-            alert.textContent = "";
-        }, 2000)
-    } else if (url.status === 200) {
-        const respuesta = await url.json();
-
-        draw(respuesta);
-
-    }
-}
-
-function draw(data) {
-    console.log(data)
-    resultado.innerHTML = `<p> Ciudad: ${data.name} <br>
-    Estado: ${data.weather[0].description} <br>
-    Temperatura: ${data.main.temp}°C <br>
-    Sensación termica: ${data.main.feels_like}°C <br>
-    Humedad: ${data.main.humidity}% <br>
-    Viento: ${data.wind.speed} M/S
-    </p> `;
-}
---
+++ js/chart.js
@@ -0,0 +1,25 @@
+
+    var ctx = document.getElementById('myChart');
+
+    function drawChart(chartData) {
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Temperatura Min', 'Temperatura Max'],
+                datasets: [{
+                    label: 'Grafica de temp min & max en la ciudad',
+                    data: chartData,
+                    backgroundColor: [
+                        'rgba(252, 191, 30, 1)',
+                        'rgba(54, 162, 235, 0.2)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+
+            }
+        });
+    }
+
+
